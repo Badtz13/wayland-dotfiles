33470fe70c056cf2b6292455a1c56a7d8c68c3b0
Badtz <badtzmaru139@gmail.com>
2024-02-19 11:01:25 -0800

    allow for theme randomization, and bind to randomize

--- .config/hypr/theme.sh
+++ .config/hypr/theme.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# If "random" is passed, select a random theme folder
+if [ "$1" == "random" ]; then
+  themes=($HOME/Pictures/wallpapers/*)
+  rand_theme=${themes[RANDOM % ${#themes[@]}]}
+  theme=$(basename "$rand_theme")
+else
+  theme=$1
+fi
+
 # locate theme images, find proper file extensions 
 function find_image {
   local theme=$1
@@ -17,18 +26,17 @@ function find_image {
   return 1
 }
 
-# make sure images were found 
-image1=$(find_image $1 1)
-image2=$(find_image $1 2)
-image3=$(find_image $1 3)
-
-# generate pywal scheme
-[ "$image1" != "Error: No image found." ] && wal -q -i "$image1" 
+# Adjust the theme variable for image finding
+image1=$(find_image $theme 1)
+image2=$(find_image $theme 2)
+image3=$(find_image $theme 3)
 
 # set wallpapers if they were found
 [ "$image1" != "Error: No image found." ] && hyprctl hyprpaper preload "$image1" 
 [ "$image2" != "Error: No image found." ] && hyprctl hyprpaper preload "$image2" 
 [ "$image3" != "Error: No image found." ] && hyprctl hyprpaper preload "$image3" 
+
+sleep 0.5
 
 hyprctl hyprpaper wallpaper "DP-2, $image1"
 hyprctl hyprpaper wallpaper "DP-3, $image2"
@@ -39,6 +47,9 @@ hyprctl hyprpaper unload all
 # generate gtk theme
 wpg -s $image1
 
+# generate pywal scheme
+[ "$image1" != "Error: No image found." ] && wal -q -i "$image1" 
+
 # run pywal scripts
 pywalfox update
 sh $HOME/.config/dunst/wal.sh
@@ -47,7 +58,7 @@ pywal-discord -t default
 $HOME/.config/wpg/wp_init.sh
 
 # store current theme
-echo $1 > $HOME/.config/hypr/CurrentTheme
+echo $theme > $HOME/.config/hypr/CurrentTheme
 
 # notify theme
-notify-send "Theme set to" $(cat ~/.config/hypr/CurrentTheme ) 
+notify-send "Theme set to" $(cat ~/.config/hypr/CurrentTheme)
