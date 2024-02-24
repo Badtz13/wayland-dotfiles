bbb29e57ad94e29b3d68f782a5ecec7bb61ea814
Badtz <badtzmaru139@gmail.com>
2024-02-24 12:35:34 -0800

    move theme to local dir, add script to establish symlink, update paths

---
+++ .config/sddm/wal.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# symlink sddm conf
+ln -sf ~/.cache/wal/theme.conf ~/.config/sddm/themes/where_is_my_sddm_theme/theme.conf
--- .config/wal/setup_sddm.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# symlink sddm conf
-sudo ln -sf ~/.cache/wal/theme.conf /usr/share/sddm/themes/where_is_my_sddm_theme/theme.conf
