9e92f8f448cfc9343c5b56c0bee7885061f2c779
Badtz <badtzmaru139@gmail.com>
2024-04-07 12:13:36 -0700

    update modelines, fix kitty font, remove old rules

--- .config/sddm/wal.sh
+++ .config/sddm/wal.sh
@@ -4,3 +4,4 @@
 cp ~/.cache/wal/theme.conf ~/.config/sddm/themes/where_is_my_sddm_theme/theme.conf
 
 sudo cp -r /home/badtz/.config/sddm/themes/where_is_my_sddm_theme /usr/share/sddm/themes/
+echo $(date) > /home/badtz/.config/sddm/log.txt
