beb596e36b7ba08e8db31bb75aa9a400413ee274
Badtz <badtzmaru139@gmail.com>
2024-04-16 19:51:52 -0700

    use rosepine-cursor, hyprcursor, blur layers

--- .config/scripts/notify_time.sh
+++ .config/scripts/notify_time.sh
@@ -3,5 +3,5 @@
 export DISPLAY=":0"
 export XDG_RUNTIME_DIR="/run/user/$(id -u)"
 export DBUS_SESSION_BUS_ADDRESS="unix:path=${XDG_RUNTIME_DIR}/bus"
-notify-send "Current Time" "$(date +"%-I:%M%p:%S")" -t 0
+notify-send "Current Time" "$(date +"%-I:%M%p:%S")" -t 15000
 paplay /home/badtz/.config/scripts/gong.ogg
