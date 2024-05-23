562beadd7ecf8e9afac47fb8a0ba9967e80d68d5
Badtz <badtzmaru139@gmail.com>
2024-05-23 12:16:29 -0700

    remove sddm, add mirrors function, remove league fules, add lutris rules, update dunst inactivity

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,7 @@ set -x LC_ALL en_US.UTF-8
 complete -f -c theme  # Remove all existing completions for 'theme'
 complete -c theme -a '(__theme_dirs)'  # Add your specific completion
 cat ~/.cache/wal/sequences
+
+if test -z "$DISPLAY" && test (tty) = "/dev/tty1"
+    exec Hyprland
+end
---
+++ .config/fish/functions/mirrors.fish
@@ -0,0 +1,3 @@
+function mirrors
+ rate-mirrors arch | sudo tee /etc/pacman.d/mirrorlist; 
+end
