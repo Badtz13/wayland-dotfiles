5039c2807de438f35b414a760b5e5e787a862c3c
Badtz <badtzmaru139@gmail.com>
2024-06-26 12:05:02 -0700

    update hypr config, clean up scripts

--- .config/scripts/notify_time.sh
+++ .config/scripts/notify_time.sh
@@ -4,4 +4,4 @@ export DISPLAY=":0"
 export XDG_RUNTIME_DIR="/run/user/$(id -u)"
 export DBUS_SESSION_BUS_ADDRESS="unix:path=${XDG_RUNTIME_DIR}/bus"
 notify-send "Current Time" "$(date +"%-I:%M%p")" -t 15000
-paplay /home/badtz/.config/scripts/gong.ogg
+paplay /home/badtz/.config/scripts/gong.ogg --volume 32536
